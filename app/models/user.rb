36e12e20d3965d4dee7f001ab7561ca3948499b3
Alex Bujenita <dekc89@gmail.com>
2019-05-15 09:47:53 +0100

    resource Message added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :photos
+  has_many :messages
 
   validates :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_141747) do
+ActiveRecord::Schema.define(version: 2019_05_15_084645) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer "user_id"
