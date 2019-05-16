17e5e33619007c98ef85473e9276d62ac3dcfe9b
Alex Bujenita <dekc89@gmail.com>
2019-05-16 16:57:06 +0100

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create!(name: 'AAA', email: 'a@a.a', instagram: 'https://www.instagram.com/fabrizioferronato/', flickr: 'https://www.flickr.com/photos/swedeitis/', avatar: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/ava1.png', avatar_filename: 'ava1.png', bio: 'Looking forward to...', password: 'aaa' )
+# User.create!(name: 'BBB', email: 'b@b.b', instagram: 'https://www.instagram.com/giorgia.salmaso/', flickr: 'https://www.flickr.com/photos/alexverweij/', avatar: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/ava2.png', avatar_filename: 'ava2.png', bio: 'Bjabdiabdiua ab diaudi hbasiudf baiufiuhiuasbdias d ai iu iuahiusbdiuabsdiu hiuh', password: 'bbb' )
+# User.create!(name: 'CCC', email: 'c@c.c', instagram: 'https://www.instagram.com/katia_parise/', flickr: 'https://www.flickr.com/photos/nelzajamal/', avatar: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/ava3.png', avatar_filename: 'ava3.png', bio: 'SUDBiuaiu uia iau hsdiuhaiysudgauysgduya gsduyagdfuyagdfuyefeur', password: 'ccc' )
+
+# Photo.create!(user_id: 4, name: 'John the green', caption: 'Me hungry...', aws_filename: 'mac1.jpg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/mac1.jpg')
+# Photo.create!(user_id: 4, name: 'Jasdasd', caption: 'Me hunrgresgregreggry...', aws_filename: 'mac2.jpg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/mac2.jpg')
+
+# Photo.create!(user_id: 5, name: 'Po', caption: 'Me hrgergegreggry...', aws_filename: 'por1', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/por1')
+# Photo.create!(user_id: 5, name: 'adaAdua', caption: 'Mhrdthesgregreggry...', aws_filename: 'por2.jpg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/por2.jpg')
+# Photo.create!(user_id: 5, name: 'EWJYG', caption: 'Me rhsgresgregreggry...', aws_filename: 'por3.jpg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/por3.jpg')
+
+# Photo.create!(user_id: 6, name: 'NATTT', caption: 'Smokin big ol doinks...', aws_filename: 'land1.jpeg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/land1.jpeg')
+# Photo.create!(user_id: 6, name: 'IFBEe', caption: 'Me see mucho grass', aws_filename: 'land2.jpg', aws_url: 'https://s3.eu-west-1.amazonaws.com/test-bucket-oioa/land2.jpg')
