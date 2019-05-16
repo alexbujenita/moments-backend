e93e1b319edfc080dff081ffeac1566d44cf02be
Alex Bujenita <dekc89@gmail.com>
2019-05-16 17:44:26 +0100

    serialized user and added route for show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-
   def index
     users = User.all
     render json: users
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    photos = user.photos
+    user = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      instagram: user.instagram,
+      flickr: user.flickr,
+      avatar: user.avatar,
+      avatar_filename: user.avatar_filename,
+      bio: user.bio,
+      photos: photos
+    }
+    render json: user
   end
 
   def create
@@ -18,6 +34,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def users_photos(user)
+    user.photos
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "users/:id", to: "users#show"
   get "users/all", to: "users#index"
   post 'users/create', to: "users#create"
   post "/auth/create", to: "auth#create"
