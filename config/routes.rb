adfb11600dab1583eb6352e127257372642d5326
Alex Bujenita <dekc89@gmail.com>
2019-05-17 09:17:36 +0100

    fixed route priority

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "users/:id", to: "users#show"
   get "users/all", to: "users#index"
   post 'users/create', to: "users#create"
+  get "users/:id", to: "users#show"
   post "/auth/create", to: "auth#create"
   get "/auth/show", to: "auth#show"
   
