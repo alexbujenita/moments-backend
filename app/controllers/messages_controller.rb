ab985b90a7e5df76448cfe76b31e88148caa30fe
Alex Bujenita <dekc89@gmail.com>
2019-05-18 14:59:04 +0100

    messages can be created

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
+# Messages controller class
 class MessagesController < ApplicationController
   def create
-    message = Message.create!(content: params[:content], user_id: params[:user_id])
+    Message.create!(content: params[:content], user_id: params[:user_id])
     render json: { ok: true }
   end
 end
